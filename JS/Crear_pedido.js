43c50c8000738650bdf3775b955e25b9cfc8e46d
JhonatanRiBu <jhonatan.riquelme.b@uni.pe>
2021-11-04 04:42:11 -0500

    popup v1

--- JS/Crear_pedido.js
+++ JS/Crear_pedido.js
@@ -5,3 +5,59 @@ $(document).ready(function () {
         "paging": false
     });
 });
+/*Popup*/
+var btnAbrirpopup1 = document.getElementById('btn-neon_cliente'),
+    overlay = document.getElementById("overlay"),
+    popup1 = document.getElementById("popup1"),
+    btnCerrarpopup1 = document.getElementById("btn-cerrar-pop-up1");
+
+btnAbrirpopup1.addEventListener('click', function () {
+    overlay.classList.add("activate");
+});
+btnCerrarpopup1.addEventListener('click', function () {
+    overlay.classList.remove("activate");
+});
+/*Ocultar boton_busqueda*/
+var nomostrar1 = document.getElementById('nomostrar1'),
+    nomostrar1 = document.getElementById('nomostrar1'),
+    nomostrar1 = document.getElementById('nomostrar1'),
+    nomostrar1 = document.getElementById('nomostrar1'),
+    nomostrar1 = document.getElementById('nomostrar1'),
+    nomostrar1 = document.getElementById('nomostrar1');
+btnAbrirpopup1.addEventListener('click', function () {
+    nomostrar1.classList.add("activate");
+});
+btnAbrirpopup1.addEventListener('click', function () {
+    nomostrar2.classList.add("activate");
+});
+btnAbrirpopup1.addEventListener('click', function () {
+    nomostrar3.classList.add("activate");
+});
+btnAbrirpopup1.addEventListener('click', function () {
+    nomostrar4.classList.add("activate");
+});
+btnAbrirpopup1.addEventListener('click', function () {
+    nomostrar5.classList.add("activate");
+});
+btnAbrirpopup1.addEventListener('click', function () {
+    nomostrar6.classList.add("activate");
+});
+
+btnCerrarpopup1.addEventListener('click', function () {
+    nomostrar1.classList.remove("activate");
+});
+btnCerrarpopup1.addEventListener('click', function () {
+    nomostrar2.classList.remove("activate");
+});
+btnCerrarpopup1.addEventListener('click', function () {
+    nomostrar3.classList.remove("activate");
+});
+btnCerrarpopup1.addEventListener('click', function () {
+    nomostrar4.classList.remove("activate");
+});
+btnCerrarpopup1.addEventListener('click', function () {
+    nomostrar5.classList.remove("activate");
+});
+btnCerrarpopup1.addEventListener('click', function () {
+    nomostrar6.classList.remove("activate");
+});
