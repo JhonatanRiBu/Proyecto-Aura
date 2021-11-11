ed3dbe498badd2c3049152a10d6c22993ea59605
JhonatanRiBu <jhonatan.riquelme.b@uni.pe>
2021-11-10 23:27:39 -0500

    crear_cotizacion v2

---
+++ JS/crear_cotizacion.js
@@ -0,0 +1,56 @@
+/*Popup*/
+var btnAbrirpopup1 = document.getElementById('cliente'),
+    overlay1 = document.getElementById("overlay1"),
+    popup1 = document.getElementById("popup1"),
+    btnCerrarpopup1 = document.getElementById("btn-cerrar-pop-up1"),
+    btnAgregarCliExistente = document.getElementById("btn_agregar_cli_existente");
+
+
+btnAbrirpopup1.addEventListener('click', function () {
+    overlay1.classList.add("activate");
+    popup1.classList.add("activate");
+});
+btnCerrarpopup1.addEventListener('click', function () {
+    overlay1.classList.remove("activate");
+    popup1.classList.remove("activate");
+});
+btn_agregar_cli_existente.addEventListener('click', function () {
+    overlay1.classList.remove("activate");
+    popup1.classList.remove("activate");
+});
+
+
+/*Ocultar boton_busqueda*/
+var nomostrar1 = document.getElementById('nomostrar1'),
+    nomostrar2 = document.getElementById('nomostrar2'),
+    nomostrar3 = document.getElementById('nomostrar3');
+    
+btnAbrirpopup1.addEventListener('click', function () {
+    nomostrar1.classList.add("activate");
+});
+btnAbrirpopup1.addEventListener('click', function () {
+    nomostrar2.classList.add("activate");
+});
+btnAbrirpopup1.addEventListener('click', function () {
+    nomostrar3.classList.add("activate");
+});
+
+
+btnCerrarpopup1.addEventListener('click', function () {
+    nomostrar1.classList.remove("activate");
+});
+btnCerrarpopup1.addEventListener('click', function () {
+    nomostrar2.classList.remove("activate");
+});
+btnCerrarpopup1.addEventListener('click', function () {
+    nomostrar3.classList.remove("activate");
+});
+btnAgregarCliExistente.addEventListener('click', function () {
+    nomostrar1.classList.remove("activate");
+});
+btnAgregarCliExistente.addEventListener('click', function () {
+    nomostrar2.classList.remove("activate");
+});
+btnAgregarCliExistente.addEventListener('click', function () {
+    nomostrar3.classList.remove("activate");
+});
