12226a62e7d3252cc8bab8ecbf77d02a859675e1
JhonatanRiBu <jhonatan.riquelme.b@uni.pe>
2021-11-17 23:04:34 -0500

    modificaciones para la bd

--- db/Parte3_Jhonatan.sql
+++ db/Parte3_Jhonatan.sql
@@ -1,34 +1,33 @@
 create table comprobante(
-    CodComprobante varchar(20),
+    CodComprobante integer,
     FechaFacturacion date,
     MontoTotal numeric(9,2),
-    CodPedido varchar(50),
-    CodTipoComprobante varchar(50),   
-    CodParty varchar(50),
+    CodPedido integer,
+    CodTipComprobante varchar(50),   
+    CodParty integer,
     SubMontoTotal numeric(9,2),
     IGV numeric(9,2),
     FechaVencimiento date,
 );
 create table TipoComprobante(
-    CodTipoComprobante varchar(50),
+    CodTipComprobante varchar(50),
     Descripcion varchar(50)
 );
 create table ReportexComprobante(
-    CodComprobante varchar(50),
-    CodReporteVentas varchar(50),
+    CodComprobante integer,
+    CodReporteVentas integer,
 );
 create table ReporteVentas(
-    CodReporteVentas varchar(50),
+    CodReporteVentas integer,
     FechaReporte date,
     MontoTotal numeric(9,2),
-    CodMoneda varchar(50),
     FechaInicio date,
     FechaFin date,
     TipoJoya varchar(50)
 );
 create table ItemComprobante(
     CodProducto varchar(50),
-    CodComprobante varchar(50),
+    CodComprobante integer,
     Cantidad Integer
 );
 create table StockProducto(
