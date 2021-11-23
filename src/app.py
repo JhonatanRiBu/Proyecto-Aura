0340ea1faa3588c4936d5aa2400a6a3121fde207
J3SSUS <jpurizagan@gmail.com>
2021-11-22 19:06:29 -0500

    python

--- src/app.py
+++ src/app.py
@@ -3,9 +3,33 @@ from flask import Flask, render_template, request, flash, redirect, url_for, ses
 
 app = Flask(__name__)
 
+# Jesus
+
+
 @app.route('/iniciosesion')
 def InicioSesion():
     return render_template('InicioSesion.html')
+
+
+
+# Jhonatan
+
+
+
+
+
+
+
+
+
+
+#Iosif
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
