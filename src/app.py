7dd98dad26302d723e5eb84ac425a4fff44d2e4c
JhonatanRiBu <jhonatan.riquelme.b@uni.pe>
2021-11-25 11:23:59 -0500

    fzassssso

--- src/app.py
+++ src/app.py
@@ -1,8 +1,13 @@
+from re import S
 from flask import Flask, render_template, request, flash, redirect, url_for, session
-
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-
+app.config['MYSQL_HOST'] = 'bkstiyllpyyyrxlek7bt-mysql.services.clever-cloud.com'
+app.config['MYSQL_USER'] = 'utdk0pjqeflx9aib'
+app.config['MYSQL_PASSWORD'] = 'W3W4O1uOe9gGRFHBJINn'
+app.config['MYSQL_DB'] = 'bkstiyllpyyyrxlek7bt'
+mysql = MySQL(app)
 # Jesus
 
 
@@ -15,7 +20,12 @@ def InicioSesion():
 # Jhonatan
 @app.route('/Crear_Pedido')
 def Crear_Pedido():
-    return render_template('Crear_Pedido.html')
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM Producto')
+    data = cur.fetchall()
+    print(data)
+
+    return render_template('Crear_Pedido.html', Productos = data)
 
 
 
